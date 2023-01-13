5e3a5b637ee0372037ca7a1e4d0d4e0df68b75fc
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-01-13 16:04:10 +0100

    Initial works

---
+++ source/uim/http/clients/auths/basic.d
@@ -0,0 +1,61 @@
+module uim.cake.http.clients\Auth;
+
+@safe:
+import uim.cake;
+
+/**
+ * Basic authentication adapter for Cake\Http\Client
+ *
+ * Generally not directly constructed, but instead used by {@link uim.cake.Http\Client}
+ * when $options["auth"]["type"] is "basic"
+ */
+class Basic
+{
+    /**
+     * Add Authorization header to the request.
+     *
+     * @param uim.cake.http.Client\Request $request Request instance.
+     * @param array $credentials Credentials.
+     * returns DHTPRequest The updated request.
+     * @see https://www.ietf.org/rfc/rfc2617.txt
+     */
+    function authentication(Request $request, array $credentials): Request
+    {
+        if (isset($credentials["username"], $credentials["password"])) {
+            $value = _generateHeader($credentials["username"], $credentials["password"]);
+            /** var DHTP.Client\Request $request */
+            $request = $request.withHeader("Authorization", $value);
+        }
+
+        return $request;
+    }
+
+    /**
+     * Proxy Authentication
+     *
+     * @param uim.cake.http.Client\Request $request Request instance.
+     * @param array $credentials Credentials.
+     * returns DHTPRequest The updated request.
+     * @see https://www.ietf.org/rfc/rfc2617.txt
+     */
+    function proxyAuthentication(Request $request, array $credentials): Request
+    {
+        if (isset($credentials["username"], $credentials["password"])) {
+            $value = _generateHeader($credentials["username"], $credentials["password"]);
+            /** var DHTP.Client\Request $request */
+            $request = $request.withHeader("Proxy-Authorization", $value);
+        }
+
+        return $request;
+    }
+
+    /**
+     * Generate basic [proxy] authentication header
+     *
+     * @param string $user Username.
+     * @param string $pass Password.
+     */
+    protected string _generateHeader(string $user, string $pass) {
+        return "Basic " ~ base64_encode($user ~ ":" ~ $pass);
+    }
+}
